2099ef973b600583fb4212ce5de96b0d82d29bcd
DmitriyFrolov2 <dmitrii.frolov2018@yandex.ru>
2025-11-03 21:38:18 +0300

    Добавил builder для HTTP-клиент

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Hello")
+import httpx
+
+headers = {"Authorization": "Bearer my_secret_token"}
+
+response = httpx.get("https://httpbin.org/get", headers=headers)
+
+print(response.json())  # Заголовки включены в ответ
