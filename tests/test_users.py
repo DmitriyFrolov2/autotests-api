15d0f3b21071df06bdf31043db10d41fce3f79fb
DmitriyFrolov2 <dmitrii.frolov2018@yandex.ru>
2026-02-02 16:36:05 +0300

    Параметризация Pytest Практика работы с pytest параметризацией

--- tests/test_users.py
+++ tests/test_users.py
@@ -13,12 +13,14 @@ from tools.fakers import fake
 
 @pytest.mark.users
 @pytest.mark.regression
-@pytest.mark.parametrize("email", [
-    fake.email(domain="mail.ru"),
-    fake.email(domain="gmail.com"),
-    fake.email(domain="example.com")
+@pytest.mark.parametrize("domain", [
+    "mail.ru",
+    "gmail.com",
+    "example.com"
 ])
-def test_create_user(email: str, public_users_client: PublicUsersClient):
+def test_create_user(domain: str, public_users_client: PublicUsersClient):
+    email = fake.email(domain=domain)
+
     request = CreateUserRequestSchema(email=email)
     response = public_users_client.create_user_api(request)
     response_data = CreateUserResponseSchema.model_validate_json(response.text)
